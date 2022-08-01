910f79cc84c6f648551a89780f6d11dd31d5193c
Aleksandr <it.kucherov@gmail.com>
2022-08-01 11:48:16 +0300

    timer and bigimages

--- src/js/main.js
+++ src/js/main.js
@@ -3,9 +3,12 @@ import modals from './modules/modals';
 import tabs from './modules/tabs';
 import forms from './modules/forms';
 import changeModalState from './modules/changeModalState';
+import timer from './modules/timer';
+import images from './modules/images';
 
 window.addEventListener('DOMContentLoaded', () => {
-	const modalState = {};
+	let modalState = {};
+	let deadline = '2022-09-01';
 
 	changeModalState(modalState);
 
@@ -25,4 +28,6 @@ window.addEventListener('DOMContentLoaded', () => {
 		'inline-block'
 	);
 	forms(modalState);
+	timer('.container1', deadline);
+	images();
 });
